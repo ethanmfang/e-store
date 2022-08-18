8ee2f2782312362f4405f44533d187186079be5f
Ethan Fang <ethanmfang@gmail.com>
2022-08-18 11:40:17 -0600

    Component Refactor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import SearchIcon from "@mui/icons-material/Search";
-import { Modal } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
 import "./App.css";
+import SearchBar from "./components/SearchBar";
+import Products from "./components/Products";
 
 const getSearchedItems = (keyword, products) => {
   const search = keyword.toLowerCase();
@@ -30,15 +21,10 @@ function App() {
   const [cart, setCart] = useState([]);
   const [keyword, setKeyword] = useState("");
   const [products, setProducts] = useState([]);
-  //const [item, setItem] = useState([]);
   //const [open, setOpen] = useState(false);
 
   // const closeItemModal = () => setOpen(false);
   // const openItemModal = () => setOpen(true);
-
-  // const openItemModal = () => {
-  //   console.log("clicked");
-  // };
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -48,57 +34,12 @@ function App() {
 
   const searchedItems = getSearchedItems(keyword, products);
 
-  // const addCartCount = (cartCount, item) => {
-  //   setCartCount(cartCount + 1);
-  //   console.log([cartCount]);
-  // };
-
   return (
     <>
       <NavBar cart={cart} />
-      <Box
-        className="SearchBar"
-        component="form"
-        noValidate
-        autoComplete="off"
-        sx={{ mt: 2, mb: 2 }}
-      >
-        <TextField
-          label="Search Products"
-          variant="outlined"
-          onChange={(e) => setKeyword(e.target.value)}
-        />
-        <IconButton className="SearchButton" size="large">
-          <SearchIcon />
-        </IconButton>
-      </Box>
+      <SearchBar setKeyword={setKeyword} />
+      <Products searchedItems={searchedItems} cart={cart} setCart={setCart} />
 
-      <div className="productContainer">
-        {searchedItems.map((item) => {
-          return (
-            <Card
-              className="Card"
-              sx={{ width: 350 }}
-              key={item.id}
-              //onClick={openItemModal}
-            >
-              <CardHeader
-                avatar={<Avatar alt="product-icon" src={item.image} />}
-                action={
-                  <IconButton onClick={() => setCart([...cart, item])}>
-                    <AddIcon />
-                  </IconButton>
-                }
-                title={<Typography className="title">{item.title}</Typography>}
-                subheader={
-                  <Typography className="price">${item.price}</Typography>
-                }
-              ></CardHeader>
-              <CardMedia component="img" image={item.image} />
-            </Card>
-          );
-        })}
-      </div>
       {/* <Modal open={open} onClose={closeItemModal}>
         <Card>
           <CardHeader
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,37 +1,28 @@
-import { React, useState, useEffect, useRef } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import { Modal } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import SearchIcon from "@mui/icons-material/Search";
-
 import "./Products.css";
 
-export default function Products({ products }) {
-  const openItemModal = () => {
-    console.log("clicked");
-  };
-
+export default function Products({ searchedItems, cart, setCart }) {
   return (
     <div className="productContainer">
-      {products.map((item) => {
+      {searchedItems.map((item) => {
         return (
           <Card
             className="Card"
             sx={{ width: 350 }}
             key={item.id}
-            onClick={openItemModal}
+            //onClick={openItemModal}
           >
             <CardHeader
               avatar={<Avatar alt="product-icon" src={item.image} />}
               action={
-                <IconButton aria-label="settings">
+                <IconButton onClick={() => setCart([...cart, item])}>
                   <AddIcon />
                 </IconButton>
               }
