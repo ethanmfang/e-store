a78fbe5b98809745584b6201378b0a701a489d1a
Ethan Fang <ethanmfang@gmail.com>
2022-08-26 11:45:46 -0600

    css cleanup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import IconButton from "@mui/material/IconButton";
-import "./SearchBar.css";
 
 export default function SearchBar({ setKeyword }) {
   return (
